05a5a8e9a1cb59da597f4049acda1874a8380f61
dhieb-dev <dhiebrouissidev@gmail.com>
2026-07-08 13:06:51 +0100

    Add files via upload

---
+++ translations.js
@@ -0,0 +1,24 @@
+// translations.js
+export const translations = {
+  "ar": { "quran": "القرآن الكريم", "radios": "الإذاعات", "moshaf_count": "عدد المصاحف", "reciter": "القارئ", "moshaf": "المصحف", "surah": "السورة" },
+  "eng": { "quran": "Holy Quran", "radios": "Radios", "moshaf_count": "Number of Mushafs", "reciter": "Reciter", "moshaf": "Mushaf", "surah": "Surah" },
+  "fr": { "quran": "Le Saint Coran", "radios": "Radios", "moshaf_count": "Nombre de Mushafs", "reciter": "Récitant", "moshaf": "Mushaf", "surah": "Sourate" },
+  "ru": { "quran": "Священный Коран", "radios": "Радио", "moshaf_count": "Количество Мусхафов", "reciter": "Чтец", "moshaf": "Мусхаф", "surah": "Сура" },
+  "de": { "quran": "Heiliger Koran", "radios": "Radios", "moshaf_count": "Anzahl der Mushafs", "reciter": "Rezitator", "moshaf": "Mushaf", "surah": "Sure" },
+  "es": { "quran": "El Sagrado Corán", "radios": "Radios", "moshaf_count": "Número de Mushafs", "reciter": "Recitador", "moshaf": "Mushaf", "surah": "Sura" },
+  "tr": { "quran": "Kur'an-ı Kerim", "radios": "Radyolar", "moshaf_count": "Mushaf Sayısı", "reciter": "Kari", "moshaf": "Mushaf", "surah": "Sure" },
+  "cn": { "quran": "古兰经", "radios": "广播", "moshaf_count": "Mushafs 数量", "reciter": "朗诵者", "moshaf": "Mushaf", "surah": "苏拉" },
+  "th": { "quran": "พระคัมภีร์อัลกุรอาน", "radios": "วิทยุ", "moshaf_count": "จำนวน Mushafs", "reciter": "ผู้อ่าน", "moshaf": "Mushaf", "surah": "ซูเราะห์" },
+  "ur": { "quran": "قرآن کریم", "radios": "ریڈیو", "moshaf_count": "مصحف کی تعداد", "reciter": "قاری", "moshaf": "مصحف", "surah": "سورہ" },
+  "bn": { "quran": "আল-কুরআন", "radios": "রেдио", "moshaf_count": "মুসহাফ সংখ্যা", "reciter": "কারী", "moshaf": "মুসহাফ", "surah": "সূরা" },
+  "bs": { "quran": "Časni Kur'an", "radios": "Radio", "moshaf_count": "Broj mushafa", "reciter": "Učač", "moshaf": "Mushaf", "surah": "Sura" },
+  "ug": { "quran": "قۇرئان كەرىم", "radios": "رادىئو", "moshaf_count": "مۇسھەف سانى", "reciter": "قارى", "moshaf": "مۇسھەف", "surah": "سۈرە" },
+  "fa": { "quran": "قرآن کریم", "radios": "رادیو", "moshaf_count": "تعداد مصحف‌ها", "reciter": "قاری", "moshaf": "مصحف", "surah": "سوره" },
+  "tg": { "quran": "Қуръони Карим", "radios": "Радио", "moshaf_count": "Шумораи Мусҳафҳо", "reciter": "Қорӣ", "moshaf": "Мусҳаф", "surah": "Сура" },
+  "ml": { "quran": "വിശുദ്ധ ഖുർആൻ", "radios": "റേഡിയോ", "moshaf_count": "മുസ്ഹഫുകളുടെ എണ്ണം", "reciter": "കാരി", "moshaf": "മുസ്ഹഫ്", "surah": "സൂറത്ത്" },
+  "tl": { "quran": "Banal na Quran", "radios": "Radyo", "moshaf_count": "Bilang ng Mushafs", "reciter": "Tagabigkas", "moshaf": "Mushaf", "surah": "Surah" },
+  "id": { "quran": "Al-Qur'an", "radios": "Radio", "moshaf_count": "Jumlah Mushaf", "reciter": "Qari", "moshaf": "Mushaf", "surah": "Surah" },
+  "pt": { "quran": "Alcorão Sagrado", "radios": "Rádios", "moshaf_count": "Número de Mushafs", "reciter": "Recitador", "moshaf": "Mushaf", "surah": "Surata" },
+  "ha": { "quran": "Alkur'ani Mai Girma", "radios": "Radiyo", "moshaf_count": "Yawan Mushaf", "reciter": "Mai Karatu", "moshaf": "Mushaf", "surah": "Surah" },
+  "sw": { "quran": "Qur'ani Tukufu", "radios": "Redio", "moshaf_count": "Idadi ya Mushaf", "reciter": "Msomaji", "moshaf": "Mushaf", "surah": "Sura" }
+};
